f69fc394fe8063348ad323aa1053889028896222
YxGuo <gyx_20170818@mail.ustc.edu.cn>
2020-04-28 23:21:44 +0800

    update .gitignore

---
+++ Lab1/cmake-build-debug/CMakeFiles/Lab1.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Lab1.dir/main.cpp.o"
+  "Lab1"
+  "Lab1.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Lab1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
